e0760915cdc2786a99f57dede39834d086adcdd4
Sabina <sabina.abbasova2909@gmail.com>
2021-04-11 23:00:08 +0400

    project 9

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -4,7 +4,40 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+
+  const  [color, setColor] = useState('')
+  const  [error, setError] = useState(false)
+  const  [list, setList] = useState(new Values('#f15025').all(10))
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    try {
+      let color = Values(color).all(10)
+      setList(color)
+    }
+    catch(error){
+      setError(true)
+      console.log(error)
+    }
+  }
+  return (
+    <>
+      <section>
+        <h3>Color generator</h3>
+        <form>
+          <input type="text" value={color} onChange={(e)=>setColor(e.target.value)} placeholder="#f15025" className={`${error ? 'error' : null}`}/>
+          <button className="btn" type="submit">Submit</button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index)=>{
+          return (
+            <SingleColor key={index} {...color} index={index} hexColor={color.hex}/>
+          )
+        })}
+      </section>
+    </>
+  )
 }
 
 export default App
