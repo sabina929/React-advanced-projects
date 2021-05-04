b0ee91f9eea3d73aa9e89a1a8d3de90776d0e5c4
Sabina <sabina.abbasova2909@gmail.com>
2021-05-04 17:46:05 +0400

    project 18

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,21 @@
-const paginate = () => {}
+const paginate = (followers) => {
+    const itemsPerPage = 10
+    const numbersOfPage = Math.ceil(followers.length/itemsPerPage)
+
+    // creating as many sequences as number of pages (numbersOfPage) from followers array 
+    // like:
+    // [
+    //    ['follower1','follower2', 'follower3','follower4', 'follower5',
+    //     'follower6','follower7','follower8','follower9','follower10'],
+    //    ['follower11','follower12','follower13','follower14','follower15']
+    //]
+    
+    const newFollowers = Array.from({legth:numbersOfPage}, (_, index)=>{
+        const start = index*itemsPerPage
+        return followers.slice(start, start + itemsPerPage)
+    })
+
+    return newFollowers
+}
 
 export default paginate
